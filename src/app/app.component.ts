3a7bb01f3cc5dd8261d4b4a4a3099bc385cb5afe
Guy Brown <guy@digitalopolis.co.uk>
2021-03-25 12:05:22 +0000

    iphone bug

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   isCollapsed = true;
   clientLogoSource:string = "https://logodix.com/logo/80482.png"
   currScrollTop:number = 0;
   currScrollDir:string;
   showHeader:boolean = true;
+
+  ngOnInit(): void {
+    window.scrollTo(0,1);
+  }
+
   closeBottomDrawer(){
     this.isCollapsed = true;
   }
